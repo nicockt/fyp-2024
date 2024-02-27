f5382e9d9181bff0027282f9200ec34c13a41a08
Nico Cheung <nico_ckt@yahoo.com.hk>
2024-02-27 13:37:12 +0800

    Change importance plot

--- models/util_visualization.py
+++ models/util_visualization.py
@@ -5,15 +5,21 @@ def plot_feature_importances(evaluation_df, model_name):
     # Get column names ending with "_importance"
     importance_columns = [col for col in evaluation_df.columns if col.endswith("_importance")]
 
+    # Calculate the average importance value for each feature
     importance_avgs = evaluation_df[importance_columns].mean(axis=0)
     cleaned_index = [index.replace("_importance", "") for index in importance_avgs.index]
     importance_avgs = pd.Series(importance_avgs.values, index=cleaned_index)
 
-    # Plot the column average as a bar chart
-    plt.figure(figsize=(8, 6))
+    # Plot the average as a bar chart
+    if len(importance_avgs) > 5:
+        plt.figure(figsize=(17, 8))
+        plt.xticks(range(len(importance_avgs)), importance_avgs.index, fontsize=10, rotation='45')
+        plt.tight_layout()
+    else:
+        plt.figure(figsize=(8, 6))
+        plt.xticks(range(len(importance_avgs)), importance_avgs.index)
     plt.bar(range(len(importance_avgs)), importance_avgs)
     plt.title(f'Feature Importance in {model_name} Model')
     plt.xlabel('Features')
     plt.ylabel('Importance')
-    plt.xticks(range(len(importance_avgs)), importance_avgs.index)  # Set x-axis tick labels
     plt.show()
