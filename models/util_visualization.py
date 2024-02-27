d9833eb56e7e9e595bc3580e96c64017b7a4239f
Nico Cheung <nico_ckt@yahoo.com.hk>
2024-02-27 14:40:34 +0800

    Plot ROC curve function

--- models/util_visualization.py
+++ models/util_visualization.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+from sklearn.metrics import auc
 
 def plot_feature_importances(evaluation_df, model_name):
     # Get column names ending with "_importance"
@@ -22,4 +24,38 @@ def plot_feature_importances(evaluation_df, model_name):
     plt.title(f'Feature Importance in {model_name} Model')
     plt.xlabel('Features')
     plt.ylabel('Importance')
+    plt.show()
+
+def plot_roc_curve(evaluation_df, model_name):
+    tprs = []
+    aucs = evaluation_df['AUC-ROC']
+    mean_fpr = np.linspace(0, 1, 100)
+
+    fig, ax = plt.subplots(figsize=(6, 6))
+
+    for i in range(len(evaluation_df)):
+        interp_tpr = np.interp(mean_fpr, evaluation_df['False Positive Rate'][i], evaluation_df['True Positive Rate'][i])
+        interp_tpr[0] = 0.0
+        tprs.append(interp_tpr)
+
+    mean_tpr = np.mean(tprs, axis=0)
+    mean_tpr[-1] = 1.0
+    mean_auc = auc(mean_fpr, mean_tpr)
+    std_auc = np.std(aucs)
+    ax.plot(
+        mean_fpr,
+        mean_tpr,
+        color="b",
+        label=r"Mean ROC (AUC = %0.2f $\pm$ %0.2f)" % (mean_auc, std_auc),
+        lw=2,
+        alpha=0.8,
+    )
+    ax.set(
+        xlabel="False Positive Rate",
+        ylabel="True Positive Rate",
+        title=f"Mean ROC curve for {model_name} Model"
+    )
+    plt.plot([0, 1], [0, 1], 'k--')  # Diagonal line
+
+    ax.legend(loc="lower right")
     plt.show()
